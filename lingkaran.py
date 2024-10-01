a5e50417e9030450854532f130ee072152a048a2
= <=>
2024-10-01 13:11:56 +0700

    100 Program Python

---
+++ lingkaran.py
@@ -0,0 +1,7 @@
+print('='*30)
+print('   keliling lingkaran   ')
+print('='*30)
+
+jari_jari = int(input('maasukkan jari jari lingkaran\t: '))
+keliling = 2 * 3.14 * jari_jari
+print('keliling lingkaran adalah\t: ', keliling)
---
+++ persegi.py
@@ -0,0 +1,11 @@
+print('='*30)
+print('      luas persegi    ')
+print('='*30)
+
+sisi = int(input('masukan nilai sisi\t: '))
+
+luas = sisi * sisi
+keliling = 4 * sisi
+
+print('luas persegi\t\t:',luas,'cm2')
+print('keliling persegi\t:',keliling'cm2')
